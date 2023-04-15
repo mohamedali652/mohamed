9846e3a6ba96e99427c15312eb9fda9747b1b8f5
mohamed <dodyali652@gmail.com>
2023-04-15 15:34:06 +0200

    mohamed

---
+++ main.js
@@ -0,0 +1,17 @@
+// var Myname = "ahmed"
+// let Agge = "19"
+// const country = "eg"
+// var Name;
+// let Name;
+// let myAge = 26
+//  myAge = 27
+//  myAge = 28
+// console.log(myAge)
+// let hhhh = "ahmed"
+// var myName = "mohamed"
+// console.log(typeof myName);
+// let num1 = 2
+// let num2 = 1
+// let result = num1 + num2
+// console.log(result);
+
